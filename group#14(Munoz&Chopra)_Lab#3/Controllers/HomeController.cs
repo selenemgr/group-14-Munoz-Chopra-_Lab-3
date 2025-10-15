37ce7c722250fc0961edde178c73fa31eee6e427
Selene Munoz <smunozgomez@advancedutility.com>
2025-10-15 11:39:31 -0400

    first commit

---
+++ group#14(Munoz&Chopra)_Lab#3/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using group_14_Munoz_Chopra__Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace group_14_Munoz_Chopra__Lab_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ group#14(Munoz&Chopra)_Lab#3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace group_14_Munoz_Chopra__Lab_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
