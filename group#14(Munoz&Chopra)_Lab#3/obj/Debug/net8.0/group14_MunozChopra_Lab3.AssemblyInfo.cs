af99bfff0016e62d0fe917ac025f162487b1ebf1
Selene Munoz <smunozgomez@advancedutility.com>
2025-10-23 00:44:25 -0400

    Remove sln duplicate

--- group#14(Munoz&Chopra)_Lab#3/obj/Debug/net8.0/group14_MunozChopra_Lab3.AssemblyInfo.cs
+++ group#14(Munoz&Chopra)_Lab#3/obj/Debug/net8.0/group14_MunozChopra_Lab3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("group14_MunozChopra_Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e009b7d0aea76b7e7ec3dcb7ea3c5cc8975d4868")]
 [assembly: System.Reflection.AssemblyProductAttribute("group14_MunozChopra_Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("group14_MunozChopra_Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
